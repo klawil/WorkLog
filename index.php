909f0ca794a49f62117dd0dd1e467d4c02b090de
klawil <wklausmeyer@yahoo.com>
2015-04-22 13:12:40 -0500

    Added slight database functionality

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
+$UN = 'TimeLog';
+$PW = 'TimeLogPass'; //Credentials to connect to MySQL
+$DB = 'TimeLog'; //DB Name - will be changed on a db by db basis
+$HOST = 'localhost';
 if ( isset($_GET['inout']) ) {
-	echo '1';
+	$DBConn = mysqli_connect($HOST, $UN, $PW, $DB);
+	if ( !$GLOBALS['DBConn'] ) {
+		echo 'Connection failed: ' . mysqli_connect_error();
+		return 0;
+	}
+	$Query = mysqli_query($DBConn,'select clocked from current;');
+	if ( !$Query ) {
+		echo 'Error - MySQL error: ' . mysqli_error($DBConn) . '.';
+		return 0;
+	}
+	$Data = mysqli_fetch_assoc($Query);
+	echo $Data['clocked'];
 	return 0;
 }
 ?>
